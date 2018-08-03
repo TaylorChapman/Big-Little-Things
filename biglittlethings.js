87ff82be6754375711d57a76b8af14413aafecb7
TaylorChapman <chapmant@bxscience.edu>
2018-08-03 16:10:43 -0400

    initial commit

---
+++ biglittlethings.js
@@ -0,0 +1,11 @@
+function dropone() {
+    document.getElementById("textone").classList.toggle("show");
+}
+
+function droptwo() {
+    document.getElementById("texttwo").classList.toggle("show");
+}
+
+function dropthree() {
+    document.getElementById("textthree").classList.toggle("show");
+}
